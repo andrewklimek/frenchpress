939ccc917bc191742d51ec389e13cccc45985ad4
andrewklimek <andrew.klimek@gmail.com>
2022-10-13 14:02:40 -0400

    a little cleanup

--- includes/extras.php
+++ includes/extras.php
@@ -7,6 +7,8 @@
  /**
   * WP adds a style attribute with width to figures when using captions shortcode to contain long captions.
   * either live with adding "figure" to the list of things getting max-width:100% or use this weird filter:
+  *
+  * should be changed in core shouldnt it? https://github.com/WordPress/WordPress/blob/c488a71e1196594088723a5753ed886a6b84d017/wp-includes/media.php#L2289
   */
 add_filter( 'img_caption_shortcode_width', 'frenchpress_img_caption_width_to_max_width' );
 function frenchpress_img_caption_width_to_max_width( $width ){
@@ -18,20 +20,17 @@ function frenchpress_img_caption_width_to_max_width( $width ){
     return $width;
 }
 
+
 /**
- * This doesnt seem smart... makes it hard to remove more text when you want to.
- * Customize the [...] at the end of excerpts
- * but... when using the_excerpt(), manual excerpts don’t get the "more" text but auto-generated excerpts do... it’s weird.
- * So I am passing a blank string (or maybe ...) to the 'excerpt_more' filter and instead adding the more link via 'wp_trim_excerpt'
+ * Below are a waste of PHP processing if pages aren't cached,
+ * so at least check for WP_CACHE which is generally defined at the top of wp-config
  */
-// add_filter( 'excerpt_more', function(){ return '&hellip;'; } );
-function frenchpress_excerpt_more( $excerpt ) {
-	return $excerpt . sprintf( ' <a class=read-more href="%1$s">%2$s</a>',
-		get_permalink( get_the_ID() ),
-		'Continue reading <span class=meta-nav>&rarr;</span>'
-	);
-}
-// add_filter( 'wp_trim_excerpt', 'frenchpress_excerpt_more' );
+if ( WP_CACHE ) :
+
+	// Remove hAtom class.. not exactly worth it if not caching
+	add_filter( 'post_class', function($classes){ return array_diff( $classes, ['hentry'] ); } );
+	
+endif;// WP_CACHE
 
 
 /**
@@ -53,8 +52,7 @@ function frenchpress_excerpt_more( $excerpt ) {
 */
 
 // as of 5.5 you can modify the prefix with this hook
-add_filter('get_the_archive_title_prefix','__return_false');
-
+add_filter('get_the_archive_title_prefix','__return_false');// SHOULD BE AN OPTION
 
 
 
@@ -64,19 +62,7 @@ add_filter('get_the_archive_title_prefix','__return_false');
  */
 if ( WP_CACHE ) :
 
-/**
- * Post classes
- *
- * @param array $classes Classes for the body element.
- * @return array
- */
-function frenchpress_post_classes( $classes ) {
 	// Remove hAtom class.. not exactly worth it if not caching
-	$classes = array_diff( $classes, ['hentry'] );
-
-	return $classes;
-}
-add_filter( 'post_class', 'frenchpress_post_classes' );
-
-
+	add_filter( 'post_class', function($classes){ return array_diff( $classes, ['hentry'] ); } );
+		
 endif;// WP_CACHE
