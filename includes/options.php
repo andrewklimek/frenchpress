06f130ff4e12f9c1aef8b0a900d03dc38badf19f
andrewklimek <andrew.klimek@gmail.com>
2023-08-05 19:36:39 +0000

    move settings page framework to standalone file

--- includes/options.php
+++ includes/options.php
@@ -14,9 +14,9 @@ function frenchpress_add_option_to_admin_bar( $bar ) {
 }
 
 
-add_action( 'rest_api_init', 'frenchpress_register_api_endpoint' );
-function frenchpress_register_api_endpoint() {
-	register_rest_route( 'frenchpress/v1', '/s', ['methods' => 'POST', 'callback' => 'frenchpress_api_options', 'permission_callback' => function(){ return current_user_can('import');} ] );
+add_action( 'rest_api_init', 'frenchpress_register_options_endpoint' );
+function frenchpress_register_options_endpoint() {
+	register_rest_route( 'frenchpress/v1', '/settings', ['methods' => 'POST', 'callback' => 'frenchpress_api_options', 'permission_callback' => function(){ return current_user_can('manage_options');} ] );
 }
 
 function frenchpress_api_options( $request ) {
@@ -32,14 +32,6 @@ function frenchpress_admin_menu() {
 }
 
 function frenchpress_options_page() {
-
-	$url = rest_url('frenchpress/v1/');
-	$nonce = "x.setRequestHeader('X-WP-Nonce','". wp_create_nonce('wp_rest') ."')";
-	?>
-<div class=wrap>
-	<h1>Frenchpress Theme Options</h1>
-	<form onsubmit="event.preventDefault();var t=this,b=t.querySelector('.button-primary'),x=new XMLHttpRequest;x.open('POST','<?php echo $url.'s'; ?>'),<?php echo $nonce; ?>,x.onload=function(){b.innerText=JSON.parse(x.response);setTimeout(function(){b.innerText='Save Changes'},9000)},x.send(new FormData(t))">
-	<?php
 
 	$fields = array_fill_keys([
 		// 'no_drawer',
@@ -123,106 +115,13 @@ function frenchpress_options_page() {
 
 	$fields['custom_css']['type'] = 'code';
 
-
+	/**
+	 *  Build Settings Page using framework in settings_page.php
+	 **/
 	$options = [ 'frenchpress' => $fields ];// can add additional options groups to save as their own array in the options table
-
-	$values = [];
-	foreach ( $options as $g => $fields ) {
-		$values += get_option( $g, [] );
-	}
-	
-	$script = '';
-	echo '<table class=form-table>';
-	foreach ( $options as $g => $fields ) {
-		// $values = get_option($g);
-		echo "<input type=hidden name='{$g}[x]' value=1>";// hidden field to make sure things still update if all options are empty (defaults)
-		foreach ( $fields as $k => $f ) {
-			if ( !empty( $f['before'] ) ) echo "<tr><th>" . $f['before'];
-			$v = isset( $values[$k] ) ? $values[$k] : '';
-			$l = isset( $f['label'] ) ? $f['label'] : str_replace( '_', ' ', $k );
-			$size = !empty( $f['size'] ) ? $f['size'] : 'regular';
-			$hide = '';
-			if ( !empty( $f['show'] ) ) {
-				if ( is_string( $f['show'] ) ) $f['show'] = [ $f['show'] => 'any' ];
-				foreach( $f['show'] as $target => $cond ) {
-					$hide = " style='display:none'";
-					$script .= "\ndocument.querySelector('#tr-{$target}').addEventListener('change', function(e){";
-					if ( $cond === 'any' ) {
-						$script .= "if( e.target.checked !== false && e.target.value )";
-						if ( !empty( $values[$target] ) ) $hide = "";
-					}
-					elseif ( $cond === 'empty' ) {
-						$script .= "if( e.target.checked === false || !e.target.value )";
-						if ( empty( $values[$target] ) ) $hide = "";
-					}
-					else {
-						$script .= "if( !!~['". implode( "','", (array) $cond ) ."'].indexOf(e.target.value) && e.target.checked!==false)";
-						if ( !empty( $values[$target] ) && in_array( $values[$target], (array) $cond ) ) $hide = "";
-					}
-					$script .= "{document.querySelector('#tr-{$k}').style.display='revert'}";
-					$script .= "else{document.querySelector('#tr-{$k}').style.display='none'}";
-					$script .= "});";
-				}
-			}
-			if ( empty( $f['type'] ) ) $f['type'] = !empty( $f['options'] ) ? 'radio' : 'checkbox';// checkbox is default
-
-			if ( $f['type'] === 'section' ) { echo "<tbody id='tr-{$k}' {$hide}>"; continue; }
-			elseif ( $f['type'] === 'section_end' ) { echo "</tbody>"; continue; }
-			else echo "<tr id=tr-{$k} {$hide}><th>";
-			
-			if ( !empty( $f['callback'] ) && function_exists( __NAMESPACE__ .'\\'. $f['callback'] ) ) {
-				echo "<label for='{$g}-{$k}'>{$l}</label><td>";
-				call_user_func( __NAMESPACE__ .'\\'. $f['callback'], $g, $k, $v, $f );
-	        } else {
-				switch ( $f['type'] ) {
-					case 'textarea':
-						echo "<label for='{$g}-{$k}'>{$l}</label><td><textarea id='{$g}-{$k}' name='{$g}[{$k}]' placeholder='' rows=8 class={$size}-text>{$v}</textarea>";
-						break;
-					case 'code':
-						echo "<label for='{$g}-{$k}'>{$l}</label><td><textarea id='{$g}-{$k}' name='{$g}[{$k}]' placeholder='' rows=8 class='large-text code'>{$v}</textarea>";
-						break;
-					case 'number':
-						$size = !empty( $f['size'] ) ? $f['size'] : 'small';
-						echo "<label for='{$g}-{$k}'>{$l}</label><td><input id='{$g}-{$k}' name='{$g}[{$k}]' placeholder='' value='{$v}' class={$size}-text type=number>";
-						break;
-					case 'radio':
-						if ( !empty( $f['options'] ) && is_array( $f['options'] ) ) {
-							echo "{$l}<td>";
-							foreach ( $f['options'] as $ov => $ol ) {
-								if ( ! is_string( $ov ) ) $ov = $ol;
-								echo "<label><input name='{$g}[{$k}]' value='{$ov}'"; if ( $v == $ov ) echo " checked"; echo " type=radio>{$ol}</label> ";
-							}
-						}
-						break;
-					case 'select':
-						if ( !empty( $f['options'] ) && is_array( $f['options'] ) ) {
-							echo "<label for='{$g}-{$k}'>{$l}</label><td><select id='{$g}-{$k}' name='{$g}[{$k}]'>";
-							echo "<option value=''></option>";// placeholder
-							foreach ( $f['options'] as $key => $value ) {
-								echo "<option value='{$key}'" . selected( $v, $key, false ) . ">{$value}</option>";
-							}
-							echo "</select>";
-						}
-						break;
-					case 'text':
-						echo "<label for='{$g}-{$k}'>{$l}</label><td><input id='{$g}-{$k}' name='{$g}[{$k}]' placeholder='' value='{$v}' class={$size}-text>";
-						break;
-					case 'checkbox':
-					default:
-						echo "<label for='{$g}-{$k}'>{$l}</label><td><input id='{$g}-{$k}' name='{$g}[{$k}]'"; if ( $v ) echo " checked"; echo " type=checkbox >";
-						break;
-				}
-			}
-			if ( !empty( $f['desc'] ) ) echo "&nbsp; " . $f['desc'];
-		}
-	}
-	if ( $script ) echo "<script>$script</script>";
-	echo '</table>';
-
-	?><p><button class=button-primary>Save Changes</button>
-	</form>
-</div>
-<?php
+	$endpoint = rest_url('frenchpress/v1/settings');
+	$title = "Frenchpress Theme Options";
+	require( __DIR__.'/settings-page.php' );// needs $options, $endpoint, $title
 }
 
 
